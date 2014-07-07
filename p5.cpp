24611489989c90a676db80b9d4b1653649ee0b3b
jokersupreme <caraza.jose598@gmail.com>
2014-07-06 20:32:03 -0500

    Update p5.cpp

--- p5.cpp
+++ p5.cpp
@@ -58,3 +58,39 @@ bool* mul(bool* output, bool input1[4], bool input2[4])
 	
 	return sum;
 }
+bool* div(bool* output, bool input1[8], bool input2[8])
+{
+	bool* temp1 = new bool[8];
+	bool* temp2 = new bool[8];
+	bool* temp3 = new bool[8];
+
+	for (int i = 0; i < 8; i++)
+	{
+		temp1[i] = 0;
+		temp2[i] = 0;
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		shiftRight(temp1, input2);
+		if (lessthan(input1, temp1));
+		{
+			sub(input1, temp1);
+			temp2 = temp2 + 1;
+		}
+
+		shiftLeft(temp3,temp2);
+	}
+	
+	output[0] = temp3[0];
+	output[1] = temp3[1];
+	output[2] = temp3[2];
+	output[3] = temp3[3];
+	output[4] = temp3[4];
+	output[5] = temp3[5];
+	output[6] = temp3[6];
+	output[7] = temp3[7];
+
+	return output;
+	
+}
